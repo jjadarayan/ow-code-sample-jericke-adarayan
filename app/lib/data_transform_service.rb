7cdcf4042312b7443f4229ae3a05c01fe423a4c4
jjadarayan <jj_adarayan@yahoo.com>
2024-11-21 22:36:11 -0500

    Commit code changes to transform csv data to json file.

--- app/lib/data_transform_service.rb
+++ app/lib/data_transform_service.rb
@@ -1,4 +1,6 @@
 require 'csv'
+require 'json'
+require 'json-schema'
 
 class DataTransformService
 
@@ -18,18 +20,91 @@ class DataTransformService
   #
   # service.transform(csv_path)
   #
-  def transform(file_path)
-    Rails.logger.info("Processing file from location: [#{file_path}]")
+  def transform(csv_file_path)
+    Rails.logger.info("Processing file from location: [#{csv_file_path}]")
     row_counter = 0
 
-    CSV.open(file_path, 'r:UTF-8', headers: true) do |csv|
-      csv.each do |csv_row|
-        Rails.logger.debug("Processing row: #{csv_row}")
+    # Define the path to the JSON file
+    json_schema_path = File.join(__dir__, '..', '..', 'spec', 'lib', 'json_schemas', "sample_part_data.json")
+
+    # Read the JSON file
+    json_schema_content = File.read(json_schema_path)
+
+    # Parse the JSON content
+    json_schema = JSON.parse(json_schema_content)
+    
+    # Define the folder and file name to save json data
+    folder_name = File.join(__dir__, '..', '..', 'spec', 'lib', 'json_doc') 
+    Dir.mkdir(folder_name) unless Dir.exist?(folder_name)
+
+    # Read the CSV file 
+    #csv_file_path = File.join(__dir__, '..', '..', 'spec', 'csv', 'sample-part-data.csv') 
+    csv_data = CSV.read(csv_file_path, headers: true);
+
+    # Process each row and create JSON document
+    csv_data.each_with_index do |row, index|
+      json_doc = {
+        "sku" => row["sku"],
+        "display_pn" => row["display_pn"],
+        "be_product_cat_name" => row["be_product_cat_name"],
+        "short_description" => row["short_description"],
+        "lead_time" => row["lead_time"],
+        "meta_description" => row["meta_description"],
+        "meta_title" => row["meta_title"],
+        "part_status" => row["part_status"],
+        "images" => [
+          {
+            "image" => row["image"],
+            "prod_drawing" => row["prod_drawing"],
+            "datasheet" => row["datasheet"],
+            "3d_model_iges" => row["3d_model_iges"]
+          }
+        ],
+        "attributes" => [
+          { "key" => "eu_rohs_y", "value" => row["eu_rohs_y"] || ""},
+          { "key" => "china_rohs", "value" => row["china_rohs"] || ""},
+          { "key" => "reach", "value" => row["reach"] || ""},
+          { "key" => "halogen_free", "value" => row["halogen_free"] || ""},
+          { "key" => "country_of_manufacture", "value" => row["country_of_manufacture"] || ""},
+          { "key" => "package_qty", "value" => row["package_qty"] || ""},
+          { "key" => "primary_pack_type", "value" => row["primary_pack_type"] || ""},
+          { "key" => "primary_pack_qty", "value" => row["primary_pack_qty"] || ""},
+          { "key" => "contact_location_filter", "value" => row["contact_location_filter"] || ""},
+          { "key" => "current_rating", "value" => row["current_rating"] || ""},
+          { "key" => "dielectric_withstanding_volt", "value" => row["dielectric_withstanding_volt"] || ""},
+          { "key" => "material_insulator", "value" => row["material_insulator"] || ""},
+          { "key" => "material_shield", "value" => row["material_shield"] || ""},
+          { "key" => "material_slider", "value" => row["material_slider"] || ""},
+          { "key" => "number_of_contacts_filter", "value" => row["number_of_contacts_filter"] || ""},
+          { "key" => "operating_temperature_range", "value" => row["operating_temperature_range"] || ""},
+          { "key" => "orientation_filter", "value" => row["orientation_filter"] || ""},
+          { "key" => "packaging", "value" => row["packaging"] || ""},
+          { "key" => "termination_style_filter", "value" => row["termination_style_filter"] || ""},
+          { "key" => "voltage_rating", "value" => row["voltage_rating"] || ""}
+        ]
+      }
+
+      # Validate JSON document against the schema
+      begin
+        Rails.logger.debug("Processing row: #{index + 1}")
+        
+        JSON::Validator.validate!(json_schema, json_doc)
+
+        # Define the full path to the file 
+        file_name = "json_doc_#{index + 1}.json"
+        file_path = File.join(folder_name, file_name)
+
+        File.open(file_path, 'w') do |file|
+            file.write(JSON.pretty_generate(json_doc))
+        end
+
+        Rails.logger.debug("JSON document #{index + 1} is valid and has been saved.")
 
         row_counter += 1
       end
     end
-    row_counter
+
+    row_counter 
   end
 
 end
--- spec/lib/data_transform_service_spec.rb
+++ spec/lib/data_transform_service_spec.rb
@@ -1,18 +1,79 @@
 require 'rails_helper'
+require 'csv'
+require 'tempfile'
+#require_relative '../csv_processor'
 
 RSpec.describe DataTransformService do
 
-  let!(:csv_path) { 'spec/csv/sample-part-data.csv'}
-  let!(:service) { DataTransformService.new}
-
-  describe '#transform' do
-
-    it 'should transform rows in the passed file from csv to JSON docs' do
-      result = service.transform(csv_path)
-      expect(result).to eql(25)
-
+    #1
+    let!(:csv_path) { 'spec/csv/sample-part-data.csv'}
+    let!(:service) { DataTransformService.new}
+    
+    describe '#transform' do
+      it 'should return the number of rows processed to equal number of rows in sample csv file' do
+        result = service.transform(csv_path)
+        expect(result).to eql(25)
+      end
+    end
+   
+    #2
+    describe '#transform' do
+      let(:csv_content) do
+        <<~CSV
+          sku,display_pn,be_product_cat_name,short_description,lead_time,meta_description,meta_title,part_status,image,prod_drawing,datasheet,3d_model_iges,eu_rohs_y,china_rohs,reach,halogen_free,country_of_manufacture,package_qty,primary_pack_type,primary_pack_qty,contact_location_filter,current_rating,dielectric_withstanding_volt,material_insulator,material_shield,material_slider,number_of_contacts_filter,operating_temperature_range,orientation_filter,packaging,termination_style_filter,voltage_rating
+          10061122131120HLF,10061122-131120HLF,0.30MM FLEX CONNECTORS,"0.30mm Flex Connector, YLL-D Series / High Retention Force Type, 13 Pos, Side Entry Surface Mount, ZIF Connector, 0.3mm",84,"Amphenol ICC is one of the leading manufacturers of FLEX CONNECTORS .  Contact us today for more details of 0.30MM FLEX CONNECTORS, part number 10061122-131120HLF.",10061122-131120HLF | FPC/FFC | Amphenol ICC,active,http://www.amphenol-icc.com/media/wysiwyg/files/pn-image/yll_d 10061122.jpg,https://cdn.amphenol-icc.com/media/wysiwyg/files/drawing/10061122.pdf,https://cdn.amphenol-icc.com/media/wysiwyg/files/documentation/datasheet/flex/flexconnectors_030mm.pdf,https://cdn.amphenol-icc.com/media/wysiwyg/files/3d/i10061122-131120hlf.zip,Yes,Yes,Yes,Yes,Japan,5000,TAP/RL,5000,Bottom,0.2A,90V AC for 1 minute,,,"Thermoplastic Resin, Natural",13,-55,Right Angle,Embossed Tape on Reel,Surface Mount,30V
+        CSV
+      end
+  
+      it 'processes sample CSV data' do
+        Tempfile.open(['test_csv', '.csv']) do |file|
+          file.write(csv_content)
+          file.rewind 
+    
+          result = service.transform(file.path)
+          expect(1).to eq(1)
+        end
+      end
     end
 
-  end
+    #3
+    describe '#transform' do
+      let(:csv_content) do
+        <<~CSV
+          sku,display_pn,be_product_cat_name,short_description,lead_time,meta_description,meta_title,part_status,image,prod_drawing,datasheet,3d_model_iges,eu_rohs_y,china_rohs,reach,halogen_free,country_of_manufacture,package_qty,primary_pack_type,primary_pack_qty,contact_location_filter,current_rating,dielectric_withstanding_volt,material_insulator,material_shield,material_slider,number_of_contacts_filter,operating_temperature_range,orientation_filter,packaging,termination_style_filter,voltage_rating
+          ,10061122-131120HLF,0.30MM FLEX CONNECTORS,"0.30mm Flex Connector, YLL-D Series / High Retention Force Type, 13 Pos, Side Entry Surface Mount, ZIF Connector, 0.3mm",84,"Amphenol ICC is one of the leading manufacturers of FLEX CONNECTORS .  Contact us today for more details of 0.30MM FLEX CONNECTORS, part number 10061122-131120HLF.",10061122-131120HLF | FPC/FFC | Amphenol ICC,active,http://www.amphenol-icc.com/media/wysiwyg/files/pn-image/yll_d 10061122.jpg,https://cdn.amphenol-icc.com/media/wysiwyg/files/drawing/10061122.pdf,https://cdn.amphenol-icc.com/media/wysiwyg/files/documentation/datasheet/flex/flexconnectors_030mm.pdf,https://cdn.amphenol-icc.com/media/wysiwyg/files/3d/i10061122-131120hlf.zip,Yes,Yes,Yes,Yes,Japan,5000,TAP/RL,5000,Bottom,0.2A,90V AC for 1 minute,,,"Thermoplastic Resin, Natural",13,-55,Right Angle,Embossed Tape on Reel,Surface Mount,30V
+        CSV
+      end
+
+      it 'raises error when sku value in csv is missing' do
+        Tempfile.open(['test_csv', '.csv']) do |file|
+          file.write(csv_content)
+          file.rewind 
+    
+          expect {service.transform(file.path)}.to raise_error(JSON::Schema::ValidationError)
+        end
+      end
+    end
+
+    #4
+    let!(:service) { DataTransformService.new}
+    
+    describe '#transform' do
+      let(:csv_content) do
+        <<~CSV
+          display_pn,be_product_cat_name,short_description,lead_time,meta_description,meta_title,part_status,image,prod_drawing,datasheet,3d_model_iges,eu_rohs_y,china_rohs,reach,halogen_free,country_of_manufacture,package_qty,primary_pack_type,primary_pack_qty,contact_location_filter,current_rating,dielectric_withstanding_volt,material_insulator,material_shield,material_slider,number_of_contacts_filter,operating_temperature_range,orientation_filter,packaging,termination_style_filter,voltage_rating
+          10061122131120HLF,10061122-131120HLF,0.30MM FLEX CONNECTORS,"0.30mm Flex Connector, YLL-D Series / High Retention Force Type, 13 Pos, Side Entry Surface Mount, ZIF Connector, 0.3mm",84,"Amphenol ICC is one of the leading manufacturers of FLEX CONNECTORS .  Contact us today for more details of 0.30MM FLEX CONNECTORS, part number 10061122-131120HLF.",10061122-131120HLF | FPC/FFC | Amphenol ICC,active,http://www.amphenol-icc.com/media/wysiwyg/files/pn-image/yll_d 10061122.jpg,https://cdn.amphenol-icc.com/media/wysiwyg/files/drawing/10061122.pdf,https://cdn.amphenol-icc.com/media/wysiwyg/files/documentation/datasheet/flex/flexconnectors_030mm.pdf,https://cdn.amphenol-icc.com/media/wysiwyg/files/3d/i10061122-131120hlf.zip,Yes,Yes,Yes,Yes,Japan,5000,TAP/RL,5000,Bottom,0.2A,90V AC for 1 minute,,,"Thermoplastic Resin, Natural",13,-55,Right Angle,Embossed Tape on Reel,Surface Mount,30V
+        CSV
+      end
+
+      it 'raises error when sku column in csv is missing' do
+        Tempfile.open(['test_csv', '.csv']) do |file|
+          file.write(csv_content)
+          file.rewind 
+    
+          expect {service.transform(file.path)}.to raise_error(JSON::Schema::ValidationError)
+        end
+      end
+    end
 
 end
